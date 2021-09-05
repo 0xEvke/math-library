8ed871879667c29d7911ade477b91752232402ac
Stevan <83223136+0xEvke@users.noreply.github.com>
2021-09-05 23:38:54 +0200

    Create serbianmath.h

---
+++ serbianmath.h
@@ -0,0 +1,20 @@
+int sabiranje(int num1, int num2) {
+        return num1 + num2;
+}
+
+int oduzimanje(int num1, int num2) {
+        return num1 - num2;
+}
+
+int deljenje(int num1, int num2) {
+        return num1 / num2;
+}
+
+int mnozenje(int num1, int num2) {
+        return num1 * num2;
+}
+
+int ostatak(int num1, int num2) {
+        return num1 % num2;
+}
+
