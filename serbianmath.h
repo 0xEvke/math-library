deb2325f92656916ec7d5ccfd3063a5cb0095e8b
Stevan <83223136+0xEvke@users.noreply.github.com>
2021-09-06 19:46:09 +0200

    Update serbianmath.h

--- serbianmath.h
+++ serbianmath.h
@@ -2,26 +2,26 @@
 
 void sabiranje(int num1, int num2) {
         int rezultat = num1+num2;
-        printf(rezultat);
+        printf("rezultat: %d", rezultat);
 }
 
 void oduzimanje(int num1, int num2) {
         int rezultat = num1-num2;
-        printf(rezultat);
+        printf("rezultat: %d", rezultat);
 }
 
 void deljenje(int num1, int num2) {
         int rezultat = num1/num2;
-        printf(rezultat);
+        printf("rezultat: %d", rezultat);
 }
 
 void mnozenje(int num1, int num2) {
         int rezultat = num1*num2;
-        printf(rezultat);
+        printf("rezultat: %d", rezultat);
 }
 
 void ostatak(int num1, int num2) {
         int rezultat = num1%num2;
-        printf(rezultat);
+        printf("rezultat: %d", rezultat);
 }
 
